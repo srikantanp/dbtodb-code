c6ce5ea8b16779212a9c8f25ea37fc4b14b0bebd
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2018-09-21 12:00:17 +0530

    Modified the Comparison Function

--- dbtodb.py
+++ dbtodb.py
@@ -26,7 +26,8 @@ def dbFetchData(conf):
              if(k=='table1'):
                  c = conn1.cursor()
                  c.execute("select * from {}".format(v))
-                 result1 = c.fetchall()			 
+                 result1 = c.fetchall()			
+				 col_name1=[tuple[0] for tuple in c.description]
              if(k=='database2'):
                  name=v+".db"
                  conn2=dbConnect(name)
@@ -34,8 +35,9 @@ def dbFetchData(conf):
              if(k=='table2'):
                  c = conn2.cursor()
                  c.execute("select * from {}".format(v))
-                 result2 = c.fetchall() 			 
-         res=comparison(result1,result2)
+                 result2 = c.fetchall() 
+				 col_name2=[tuple[0] for tuple in c.description]				 
+         res=comparison(result1,result2,col_name1,col_name2)
          convt_csv(section,res)
       except Exception as error:
          print(error)
@@ -43,9 +45,22 @@ def dbFetchData(conf):
          conn1.close()
          conn2.close()		 
          print("connection closed")
-def comparison(r1,r2):
+def comparison(result1,result2,col_name1,col_name2):
    
-       if(sorted(r1)==sorted(r2)):
-          return 'true'
-       else:
-          return 'false'
+	if len(col_name1)==len(col_name2):
+		if len(result1)==len(result2):
+			df_1 = pd.DataFrame(data=result1, columns=col_name1)
+			df_1=df_1.reindex(sorted(df_1.columns), axis=1)
+			print(df_1)
+			df_2 = pd.DataFrame(data=result2, columns=col_name2)
+			df_2=df_2.reindex(sorted(df_2.columns), axis=1)
+			print(df_2)
+			print(df_2.equals(df_1))
+		else:
+			print("Record count mismatch")
+	else:
+		print("Column count mismatch")
+		
+if __name__=="__main__":
+	conf=readConfig("conf.ini")
+    dbFetchData(conf)

3540df1b3fbb87e9be44ba9b0db7c1cf281ca827
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2018-09-21 10:07:58 +0530

    Defined the function DbFetchData to fetch the data from the Database based on Config file

--- dbtodb.py
+++ dbtodb.py
@@ -1,1 +1,22 @@
+#Function for Fetching data from the database-Edited by Krithika Gopalsamy
+def dbFetchData(conf):
+   
+   for section in conf.sections():
 
+       for k,v in conf.items(section):
+             if(k=='database1'):
+                 name=v+".db"
+                 conn1=dbConnect(name)
+                 print("connection established with%s "%name)
+             if(k=='table1'):
+                 c = conn1.cursor()
+                 c.execute("select * from {}".format(v))
+                 result1 = c.fetchall()			 
+             if(k=='database2'):
+                 name=v+".db"
+                 conn2=dbConnect(name)
+                 print("connection established with %s "%name)
+             if(k=='table2'):
+                 c = conn2.cursor()
+                 c.execute("select * from {}".format(v))
+                 result2 = c.fetchall() 	
